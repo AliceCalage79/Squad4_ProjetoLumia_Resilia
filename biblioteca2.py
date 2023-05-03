811677afc02c7dac91d814b7ae669629531a934c
AliceCalage79 <calagecamposalice@gmail.com>
2023-05-03 18:41:25 -0300

    Arquivo biblioteca2
    Arquivo para guardar as funções que serão usadas no arquivo main2

--- biblioteca2.py
+++ biblioteca2.py
@@ -1,3 +1,3 @@
-def identificaUsuario():
+def identificaUsuario(): #Função mostra boas vindas e solicita identificação do usuario
     print ('\nBem-vindo(a) a Minha Faculdade',
            '\nPor favor, me diga como gostaria de ser chamado(a): ')
