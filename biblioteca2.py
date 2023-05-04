e4e16bcc338199727cdc875d8c0aa13496d3d034
Cristhian Monteiro <cristhiansousa8@gmail.com>
2023-05-04 20:52:55 -0300

    Alimentando o código
    Inserindo perguntas e respostas do arquivo biblioteca e alimentando o diretório principal.

--- biblioteca2.py
+++ biblioteca2.py
@@ -30,3 +30,61 @@ def validarUsuario (nome,contador):
     print("\033[1;36m"+"|*************************************|"+"\033[0;0m")
     print ('\n'+"\033[1;31m"+'Opção inválida.'+"\033[0;0m"+'\n'+(nome)+','+'\nDigite novamente. Você tem mais '+'\033[1;31m'+str(contador)+'\033[0;0m'+' tentativas' '\nSuas opções são:')
 
+#CABEÇALHO DE INFORMAÇÕES DA SEGUNDA PARTE DE PERGUNTAS CONTIDA EM CADA OPÇÃO CURSO
+def segundaPergunta(curso,nome):
+    print("\033[1;36m"+"|*************************************|"+"\033[0;0m")
+    print ('\n'+(nome)+'\nVocê escolheu a graduação em '+ str(nomeCurso(int(curso)))+'\nVeja abaixo as principais dúvidas',
+           '\nPara acessar as informações, digite a opção desejada:\n')
+
+#???
+def mostraTodasDuvidas(curso):
+        todas = ''
+        for i in range (1,5,1):
+           todas = str(todas) + ('\n'+str(i)+'.'+str(duvidas(curso,(i)))+'')
+        return todas  
+ 
+#FUNÇÃO QUE EXIBE A SEGUNDA PARTE DE PERGUNTAS CONTIDA EM CADA OPÇÃO DE CURSO
+def duvidas(curso,duvida): 
+    
+    if duvida == 1 :
+        return print (f'1. Quem é o público alvo da graduação em ' + str(nomeCurso(int(curso)))+'?')
+    if duvida == 2 :
+        return print (f'2. Em que área posso atuar com a graduação em ' + str(nomeCurso(int(curso)))+'?')
+    if duvida == 3 :
+        return print (f'3. Qual a média salarial de um profissional na área de ' + str(nomeCurso(int(curso)))+'?')
+    elif duvida == 4:
+        return print ('4. Voltar')
+    
+#FUNÇÃO QUE EXIBE AS RESPOSTAS AO USUÁRIO
+def mostraResposta(duvida, curso, nome):
+    print("\033[1;36m"+"|*************************************|"+"\033[0;0m")
+    if duvida== '1':
+        if curso =='1':
+            print ('\n'+'\033[1m'+'\n[Público alvo]\n'+'\033[0m'+(nome)+','+'\n(1) Em '+str(nomeCurso(int(curso)))+', criatividade, dinâmica e paciência são características \nimportantes para quem deseja seguir a carreira.')
+            print('Saiba mais em nosso site:'+'{}|www.minhafaculdade.com|{}'.format('\033[4;34m', '\033[m'))
+        elif curso == '2':
+            print ('\n'+'\033[1m'+'\n[Público alvo]\n'+'\033[0m'+(nome)+','+'\n(1) Em '+str(nomeCurso(int(curso)))+', deve ter conhecimento sobre filosofia, lógica, política e economia, além de gostar de ler,\nrefletir e discutir todos os tipos de assuntos, com lógica e argumentação')
+            print('Saiba mais em nosso site:'+'{}|www.minhafaculdade.com|{}'.format('\033[4;34m', '\033[m'))
+        elif curso =='3':
+            print ('\n'+'\033[1m'+'\n[Público alvo]\n'+'\033[0m'+(nome)+','+'\n(1) Em '+str(nomeCurso(int(curso)))+', necessita ter alta capacidade de concentração, observação, resolução e antecipação de problemas.')
+            print('Saiba mais em nosso site:'+'{}|www.minhafaculdade.com|{}'.format('\033[4;34m', '\033[m'))
+    if duvida== '2':
+        if curso =='1':
+            print ('\n'+'\033[1m'+'\n[Área de atuação]\n'+'\033[0m'+(nome)+','+'\n(2) Em '+str(nomeCurso(int(curso)))+', você pode atuar em: Educação Escolar - Educação Especial - Pedagogia Hospitalar - Administração Escolar \nCoordenação Pedagógica - Orientação Educacional - Psicopedagogia')
+            print('Saiba mais em nosso site:'+'{}|www.minhafaculdade.com|{}'.format('\033[4;34m', '\033[m'))
+        elif curso == '2':
+            print ('\n'+'\033[1m'+'\n[Área de atuação]\n'+'\033[0m'+(nome)+','+'\n(2) Em '+str(nomeCurso(int(curso)))+', você pode atuar em: Direito Civil - Direito Ambiental - Direito Empresarial - Direito da Tecnologia da Informação - Direito do Consumidor.')
+            print('Saiba mais em nosso site:'+'{}|www.minhafaculdade.com|{}'.format('\033[4;34m', '\033[m'))
+        elif curso =='3':
+            print ('\n'+'\033[1m'+'\n[Área de atuação]\n'+'\033[0m'+(nome)+','+'\n(2) Em '+str(nomeCurso(int(curso)))+', você pode atuar em: Administração de Banco de Dados - Infraestrutura de TI - Desenvolvimento de Software - Administração de Redes - Gestor de Projetos de Sistemas.')
+            print('Saiba mais em nosso site:'+'{}|www.minhafaculdade.com|{}'.format('\033[4;34m', '\033[m'))
+    if duvida== '3':
+        if curso =='1':
+            print ('\n'+'\033[1m'+'\n[Média Salarial]\n'+'\033[0m'+(nome)+','+'\n(3) Em '+str(nomeCurso(int(curso)))+', O salário médio no Brasil é de R$ 2.073,67.')
+            print('Saiba mais em nosso site:'+'{}|www.minhafaculdade.com|{}'.format('\033[4;34m', '\033[m'))
+        elif curso == '2':
+            print ('\n'+'\033[1m'+'\n[Média Salarial]\n'+'\033[0m'+(nome)+','+'\n(3) Em '+str(nomeCurso(int(curso)))+', a média salarial de um advogado no Brasil é de R$ 4.537,62.')
+            print('Saiba mais em nosso site:'+'{}|www.minhafaculdade.com|{}'.format('\033[4;34m', '\033[m'))
+        elif curso =='3':
+            print ('\n'+'\033[1m'+'\n[Média Salarial]\n'+'\033[0m'+(nome)+','+'\n(3) Em '+str(nomeCurso(int(curso)))+', a média salarial no Brasil é de R$ 4.348,45.')
+            print('Saiba mais em nosso site:'+'{}|www.minhafaculdade.com|{}'.format('\033[4;34m', '\033[m'))
--- main2.py
+++ main2.py
@@ -1,64 +1,61 @@
 from biblioteca2 import *
 
-identificaUsuario()      # função que retorna um texto de boas vindas, solicitando identificação do usuario
-nomeUsuario =input()                         # variavel recebe o nome do usuario 
+identificaUsuario()                             # função que retorna um texto de boas vindas, solicitando identificação do usuario
+nomeUsuario =input()                            # variavel recebe o nome do usuario 
 inicio =True
 while inicio ==True:
-     primeiraPergunta(nomeUsuario)#laço que inicia o programa controlado pela variavel boleana inicio usado para opcao voltar
-     print(mostraTodoCursos())#variavel guarda a opcao de curso desejada pelo usuario
-     operacaoCurso = input()
-     contador = 5   # contador implementado para controlar o numero de vezes que o usuario coloca opcao invalida, tem 5 tentativas depois sai
+     primeiraPergunta(nomeUsuario)              #laço que inicia o programa controlado pela variavel boleana inicio usado para opcao voltar
+     print(mostraTodoCursos())                  #variavel guarda a opcao de curso desejada pelo usuario
+     opcaoCurso = input()
+     contador = 5                               #contador implementado para controlar o numero de vezes que o usuario coloca opcao invalida, tem 5 tentativas depois sai
      while opcaoCurso  not in ('1','2','3','4'):#laco que identifica se o usuario colocou uma opcao valida, e mostra as opcoes novamente
-        if contador >=2 : #condiçao que mostra se o usuario ja digitou as 5 tentativas 
-             contador = contador-1  # contador vai decrementando de 5 ate 1 tentativas
-             validarUsuario(nomeUsuario, contador) #def mostra mensagem de opcao invalida  
-             print(mostraTodoCurso)#def mostra opcoes de curso disponiveis
-             opcaoCurso = input()  #usuario vai digitando ate opcao de curso ser valida
-        else:
-             print('\n'+(nomeUsuario) +
-                  '\nVocê digitou 5 alternativas ivalidas, vamos atualizar\nAtualizandoooo....')
-            
-               #caso digite 5 vezes opcoes invalidas o sistema sai   
-             exit ()  
+          if contador >=2 :                     #condiçao que mostra se o usuario ja digitou as 5 tentativas 
+               contador = contador-1            # contador vai decrementando de 5 ate 1 tentativas
+               validarUsuario(nomeUsuario, contador) #def mostra mensagem de opcao invalida  
+               print(mostraTodoCursos())        #def mostra opcoes de curso disponiveis
+               opcaoCurso = input()             #usuario vai digitando ate opcao de curso ser valida
+          else:
+               print ('\n'+(nomeUsuario)+'\nVocê digitou por 5 vezes alternativas iválidas.\nO atendimento terá que ser encerrado. Até a próxima!')  #caso digite 5 vezes opcoes invalidas o sistema sai   
+               exit ()  
      if opcaoCurso == '4':                         #caso a opcao seja 4 o é a opcao sair, sai do sistema
-         print('Atendimento encerrado. Obrigado por sua consulta!')
-         exit()
-         # usuario ja escolheu o curso agora vai aparecer a pergunta relacionada ao curso
+          print('Atendimento encerrado. Obrigado por sua consulta!')
+          exit()
+                                                  # usuario ja escolheu o curso agora vai aparecer a pergunta relacionada ao curso
      segundaPergunta(opcaoCurso, nomeUsuario)
-     # mostra todas as duvidas relacionadas ao curso
+                                                  # mostra todas as duvidas relacionadas ao curso
      mostraTodasDuvidas(opcaoCurso)
      opcaoDuvida = input()
 
      contador = 5
      while opcaoDuvida not in ('1','2','3','4'):
-        if contador >=2:
-             contador = contador-1
-             validarUsuario(nomeUsuario, contador)
-             (mostraTodasDuvidas(opcaoCurso))
-             opcaoDuvida = input()
+          if contador >=2:
+               contador = contador-1
+               validarUsuario(nomeUsuario, contador)
+               (mostraTodasDuvidas(opcaoCurso))
+               opcaoDuvida = input()
             
-        else:
-             print('\n'+(nomeUsuario) +
-                  '\nVocê digitou 5 alternativas ivalidas, vamos atualizar\nAtualizando....')
-             exit()
+          else:
+               print('\n'+(nomeUsuario) +
+                  '\nVocê digitou por 5 vezes alternativas iválidas.\nO atendimento terá que ser encerrado. Até a próxima!')
+               exit()
                
              
      if opcaoDuvida == '4':
-             inicio = True
+          inicio = True
      else:
-         # Função que exibe as respostas sobre todas as alternativas.
-        mostraResposta(opcaoDuvida, opcaoCurso, nomeUsuario)
-        print('\nObrigado por consultar nossa plataforma',
+          # Função que exibe as respostas sobre todas as alternativas.
+          mostraResposta(opcaoDuvida, opcaoCurso, nomeUsuario)
+          print('\nObrigado por consultar nossa plataforma',
               '\nDeseja: \n1.Retornar ao início  \n2.Encerrar')
-        resposta = int(input())
+          resposta = int(input())
                          #Função que exibe as respostas sobre todas as alternativas.
              
           
-             while resposta not in ('1','2'):
+     while resposta not in ('1','2'):
                 
-                resposta =input()
-             if resposta == '1':
-                inicio = True
-             else:
-                print ('\n'+(nomeUsuario)+', obrigado por consultar nossa plataforma. Até breve!')
-                inicio = False
+          resposta =input()
+          if resposta == '1':
+               inicio = True
+          else:
+               print ('\n'+(nomeUsuario)+', obrigado por consultar nossa plataforma. Até breve!')
+               inicio = False
